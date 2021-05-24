7195beaab5913beb4e535a344717a99843341f96
altair67 <altair67@icloud.com>
2021-05-24 20:32:16 +0300

    feat: solve tasks

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,63 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    
+    if (monthNumber < 1 || monthNumber > 12) {
+        return  nil;
+    }
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *startComponents = [[NSDateComponents alloc] init];
+    startComponents.month = monthNumber;
+
+    NSDate *date = [gregorian dateFromComponents: startComponents];
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
+    [dateFormatter setLocalizedDateFormatFromTemplate:@"MMMM"];
+
+    return [dateFormatter stringFromDate: date];
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.locale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+    dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ssZ";
+    dateFormatter.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:0];
+    
+    NSDate *newDate = [dateFormatter dateFromString: date];
+    
+    NSCalendar *gregorianCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    long monthday = [gregorianCalendar component:NSCalendarUnitDay fromDate:newDate];
+    
+    return monthday;
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    
+    NSDateFormatter *dateformatter = [NSDateFormatter new];
+    dateformatter.dateFormat = @"E";
+    dateformatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"];
+        
+    return [dateformatter stringFromDate:date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
+    
+    NSDate *todayDate = [NSDate date];
+    NSCalendar *gregorianCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    
+    if ([gregorianCalendar component:NSCalendarUnitWeekOfYear fromDate:todayDate] == [gregorianCalendar component:NSCalendarUnitWeekOfYear fromDate:date]) {
+        return  YES;
+    } else {
     return NO;
+    }
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,27 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    
+    NSMutableString *result = [[NSMutableString alloc] init];
+    
+    while (string1.length != 0 && string2.length != 0) {
+        NSString *symbol1 = [string1 substringToIndex:1];
+        NSString *symbol2 = [string2 substringToIndex:1];
+        if ([symbol1 compare:symbol2] == NSOrderedAscending || [symbol1 compare:symbol2] == NSOrderedSame) {
+            [result appendString:symbol1];
+            string1 = [string1 substringFromIndex:1];
+        } else {
+            [result appendString:symbol2];
+            string2 = [string2 substringFromIndex:1];
+        }
+        
+        if (string1.length == 0) {
+            [result appendString:string2];
+        } else if (string2.length == 0) {
+            [result appendString:string1];
+        }
+    }
+    return result;
 }
 
 @end
